c50c47fde1f99dfa1bc8e34072b8f25c4acf52ed
cluster0 <fourmou.m@gmail.com>
2020-07-04 15:45:18 +0100

    generate availability endpoints

--- availability/pkg/service/transport.go
+++ availability/pkg/service/transport.go
@@ -1,5 +1,11 @@
 package availability
 
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
 type CheckAvailabilityRequest struct {
 	Item string
 }
@@ -22,3 +28,39 @@ type ReserveRequest struct {
 	Item string
 }
 
+type ReserveResponse struct {
+	Err error
+}
+
+func MakeGetAvailabilityEndpoint(svc AvailabilityService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(GetAvailabilityRequest)
+		availability, err := svc.GetAvailability(ctx, req.Item)
+		if err != nil {
+			return GetAvailabilityResponse{Count: 0, Error: err}, nil
+		}
+		return GetAvailabilityResponse{Count: availability, Error: err}, nil
+	}
+}
+
+func MakeCheckAvailabilityEndpoint(svc AvailabilityService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(CheckAvailabilityRequest)
+		available, err := svc.CheckAvailability(ctx, req.Item)
+		if err != nil {
+			return CheckAvailabilityResponse{Available: false, Error: err}, nil
+		}
+		return CheckAvailabilityResponse{Available: available, Error: err}, nil
+	}
+}
+
+func MakeReserveAvailabilityEndpoint(svc AvailabilityService) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(ReserveRequest)
+		err := svc.Reserve(ctx, req.Item)
+		if err != nil {
+			return ReserveResponse{Err: err}, nil
+		}
+		return ReserveResponse{Err: err}, nil
+	}
+}
