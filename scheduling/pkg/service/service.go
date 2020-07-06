c65069d222ef6a4a08eeea453474df66c207c3ae
cluster0 <fourmou.m@gmail.com>
2020-07-06 17:35:36 +0100

    generate the scheduling service

--- scheduling/pkg/service/service.go
+++ scheduling/pkg/service/service.go
@@ -1,7 +1,189 @@
-package service
+package scheduling
+
+import "time"
 
 // SchedulingService describes the service.
 type SchedulingService interface {
-	// Add your methods here
-	// e.x: Foo(ctx context.Context,s string)(rs string, err error)
+	InsertSchedulingLogs(logs []SchedulingLog) error
+	InsertRotationChange(rotationChange RotationChange) error
+	InsertSchedule(schedule Schedule) (int, error)
+	RemoveSchedule(scheduleID int) error
+	InsertShiftHistory(shiftHistory ShiftHistory) error
+	UpdateSchedule(schedule Schedule) error
+	UpdateEmployeeShift(employeeShift EmployeeShift) error
+	StuckShift(scheduleID int) error
+	AssignEmployeeToUrgentUnfilled(scheduleID int, employeeID int) error
+	AddNoteSchedule(scheduleID int, note string) error
+	AddNoteMasterSchedule(masterScheduleID int, note string) error
+	AddNoteMasterChange(masterChangeID int, note string) error
+	GetMasterScheduleShifts(masterScheduleID int) ([]Shift, error)
+	InsertDetailCallOut(detailCallOut DetailCallOut) error
+	InsertCallOutResults(callOutResults []CallOutResult) error
+	AssignUserToLocationSchedule(userID int, locationID int) error
+	GetShifts(startDate time.Time, endDate time.Time) ([]Shift, error)
+	PayPeriodArray(startDate time.Time, endDate time.Time) ([]time.Time, error)
+}
+
+// SchedulingLog represents a scheduling log entry.
+type SchedulingLog struct {
+	ID        int
+	Timestamp time.Time
+	Message   string
+}
+
+// RotationChange represents a change in rotation.
+type RotationChange struct {
+	ID           int
+	ScheduleID   int
+	EmployeeID   int
+	OldRotation  string
+	NewRotation  string
+	ChangeReason string
+}
+
+// Schedule represents a schedule entry.
+type Schedule struct {
+	ID          int
+	Date        time.Time
+	ShiftID     int
+	EmployeeID  int
+	LocationID  int
+	IsCancelled bool
+}
+
+// ShiftHistory represents the history of a shift.
+type ShiftHistory struct {
+	ID         int
+	ShiftID    int
+	EmployeeID int
+	StartTime  time.Time
+	EndTime    time.Time
+}
+
+// EmployeeShift represents a shift assigned to an employee.
+type EmployeeShift struct {
+	ID         int
+	EmployeeID int
+	ShiftID    int
+	Date       time.Time
+}
+
+// Shift represents a shift definition.
+type Shift struct {
+	ID         int
+	Name       string
+	StartTime  time.Time
+	EndTime    time.Time
+	LocationID int
+}
+
+// DetailCallOut represents a detailed call-out entry.
+type DetailCallOut struct {
+	ID            int
+	EmployeeID    int
+	CallOutResult string
+}
+
+// CallOutResult represents the result of a call-out.
+type CallOutResult struct {
+	ID         int
+	ShiftID    int
+	EmployeeID int
+	Result     string
+}
+
+type schedulingService struct{}
+
+func NewSchedulingService() SchedulingService {
+	return &schedulingService{}
+}
+
+func (s *schedulingService) InsertSchedulingLogs(logs []SchedulingLog) error {
+	// Implement the logic to insert scheduling logs
+	return nil
+}
+
+func (s *schedulingService) InsertRotationChange(rotationChange RotationChange) error {
+	// Implement the logic to insert rotation change
+	return nil
+}
+
+func (s *schedulingService) InsertSchedule(schedule Schedule) (int, error) {
+	// Implement the logic to insert a schedule
+	return 0, nil
+}
+
+func (s *schedulingService) RemoveSchedule(scheduleID int) error {
+	// Implement the logic to remove a schedule
+	return nil
+}
+
+func (s *schedulingService) InsertShiftHistory(shiftHistory ShiftHistory) error {
+	// Implement the logic to insert shift history
+	return nil
+}
+
+func (s *schedulingService) UpdateSchedule(schedule Schedule) error {
+	// Implement the logic to update a schedule
+	return nil
+}
+
+func (s *schedulingService) UpdateEmployeeShift(employeeShift EmployeeShift) error {
+	// Implement the logic to update an employee shift
+	return nil
+}
+
+func (s *schedulingService) StuckShift(scheduleID int) error {
+	// Implement the logic for stuck shift
+	return nil
+}
+
+func (s *schedulingService) AssignEmployeeToUrgentUnfilled(scheduleID int, employeeID int) error {
+	// Implement the logic to assign an employee to an urgent unfilled schedule
+	return nil
+}
+
+func (s *schedulingService) AddNoteSchedule(scheduleID int, note string) error {
+	// Implement the logic to add a note to a schedule
+	return nil
+}
+
+func (s *schedulingService) AddNoteMasterSchedule(masterScheduleID int, note string) error {
+	// Implement the logic to add a note to a master schedule
+	return nil
+}
+
+func (s *schedulingService) AddNoteMasterChange(masterChangeID int, note string) error {
+	// Implement the logic to add a note to a master change
+	return nil
+}
+
+func (s *schedulingService) GetMasterScheduleShifts(masterScheduleID int) ([]Shift, error) {
+	// Implement the logic to get shifts for a master schedule
+	return nil, nil
+}
+
+func (s *schedulingService) InsertDetailCallOut(detailCallOut DetailCallOut) error {
+	// Implement the logic to insert a detailed call-out
+	return nil
+}
+
+func (s *schedulingService) InsertCallOutResults(callOutResults []CallOutResult) error {
+	// Implement the logic to insert call-out results
+	return nil
+}
+
+func (s *schedulingService) AssignUserToLocationSchedule(userID int, locationID int) error {
+	// Implement the logic to assign a user to a location schedule
+	return nil
+}
+
+func (s *schedulingService) GetShifts(startDate time.Time, endDate time.Time) ([]Shift, error) {
+	// Implement the logic to get shifts for a given date range
+	return nil, nil
+}
+
+func (s *schedulingService) PayPeriodArray(startDate time.Time, endDate time.Time) ([]time.Time, error) {
+	// Implement the logic to generate a pay period array for a given date range
+	return nil, nil
 }
